d319cf02d563d3d125b05e26d5a58f0102c24337
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-09-19 15:33:05 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const billAmount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+const checkButton = document.querySelector("#check-button")
 
-console.log(billAmount.value)
+console.log(cashGiven.value)
