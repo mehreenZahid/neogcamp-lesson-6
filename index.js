912eb90df3351bf7cac4af9371955e47ab8117d7
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-09-21 15:15:00 +0530

    validated amounts

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const billAmount = document.querySelector("#bill-amount")
 const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button")
+const message = document.querySelector("#error-message")
 
-console.log(cashGiven.value)
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
+    if(billAmount.value > 0){
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value
+            calculateChange(amountToBeReturned)
+        }
+        else{
+            showMessage("The cash provided should atleast be equal to bill amount")
+        }
+    }
+    else{
+        showMessage("Invalid Bill Amount")
+    }
+})
+
+function calculateChange(){
+
+}
+
+function hideMessage(){
+    message.style.display = "none"
+}
+
+function showMessage(msg){
+    message.style.display = "block"
+    message.innerText = msg
+}
