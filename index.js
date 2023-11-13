bb5a38152e5295cb63ac72b780e7c19f2de27a64
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-13 21:15:23 +0530

    added css

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
+    if(Number(billAmount.value) > 0){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
             const amountToBeReturned = cashGiven.value - billAmount.value
             calculateChange(amountToBeReturned)
         }
